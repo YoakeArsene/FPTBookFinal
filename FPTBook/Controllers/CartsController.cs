a646a93d7a174af22e8dd592109d3d99d1e406bb
Arsene Le <chauanh1402@gmail.com>
2022-05-11 20:24:46 +0700

    Add Order view, edit Cart view

---
+++ FPTBook/Controllers/CartsController.cs
@@ -0,0 +1,73 @@
+﻿#nullable disable
+
+using BookStore.Areas.Identity.Data;
+using BookStore.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models
+{
+    public class CartsController : Controller
+    {
+        private readonly UserContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public CartsController(UserContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Carts
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> Index()
+        {
+            string thisUserId = _userManager.GetUserId(HttpContext.User);
+            var cart = await _context.Cart
+              .Include(c => c.Book)
+              .Include(c => c.User)
+              .ToListAsync();
+            return View(_context.Cart.Where(c => c.UId == thisUserId));
+        }
+
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> Increase(string isbn)
+        {
+            string thisUserId = _userManager.GetUserId(HttpContext.User);
+            Cart fromDb = _context.Cart.FirstOrDefault(c => c.UId == thisUserId && c.BookIsbn == isbn);
+
+            fromDb.Quantity++;
+            _context.Update(fromDb);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> removeItem(string isbn)
+        {
+            string thisUserId = _userManager.GetUserId(HttpContext.User);
+            Cart fromDb = _context.Cart.FirstOrDefault(c => c.UId == thisUserId && c.BookIsbn == isbn);
+
+            fromDb.Quantity--;
+            while (fromDb.Quantity != 0)
+            {
+                _context.Update(fromDb);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> Remove(string isbn)
+        {
+            string thisUserId = _userManager.GetUserId(HttpContext.User);
+            Cart fromDb = _context.Cart.FirstOrDefault(c => c.UId == thisUserId && c.BookIsbn == isbn);
+            _context.Remove(fromDb);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FPTBook/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿#nullable disable
+
+using BookStore.Areas.Identity.Data;
+using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly UserContext _context;
+        protected readonly UserManager<AppUser> _userManager;
+
+        public OrdersController(UserContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> Index()
+        {
+            string thisUserId = _userManager.GetUserId(HttpContext.User);
+            var cart = await _context.Order
+            .Include(c => c.User)
+            .ToListAsync();
+            return View(_context.Order.Where(c => c.UId == thisUserId));
+        }
+
+        [Authorize(Roles = "Seller")]
+        public async Task<IActionResult> Manager()
+        {
+            AppUser thisUser = await _userManager.GetUserAsync(HttpContext.User);
+            Store thisStore = await _context.Store.FirstOrDefaultAsync(s => s.UId == thisUser.Id);
+            OrderDetail thisOrderDetail = _context.OrderDetail.FirstOrDefault(o => o.Book.StoreId == thisStore.Id);
+            var userContext = _context.Order.Where(o => o.Id == thisOrderDetail.OrderId).Include(o => o.User);
+            return View(userContext);
+        }
+    }
+}
