9f4ee166e64a1d7b4bcb4f34a6684bf7778351f4
Anh Le Ngoc Chau <anh.le@orientsoftware.com>
2022-06-16 14:03:23 +0700

    Add Repositories folder, add IBookRepository and BookRepository

---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models;
+
+namespace FPTBook.Repositories
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetBooks();
+        Book GetBookDetails(string id);
+        void CreateBook(Book book);
+        void EditBook(Book book);
+        void DeleteBook(Book book);
+        void Save();
+        int GetNumberOfRecords();
+    }
+}
