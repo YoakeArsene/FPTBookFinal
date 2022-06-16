56eba14046693cdba5afd67d0c985bb5ed8bd4a9
Arsene <72069029+YoakeArsene@users.noreply.github.com>
2022-06-16 16:52:13 +0700

    Add ICartRepository and CartRepository (#2)
    Co-authored-by: Anh Le Ngoc Chau <anh.le@orientsoftware.com>

---
+++ Repositories/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models;
+
+namespace FPTBook.Repositories
+{
+    public interface ICartRepository
+    {
+        IEnumerable<Cart> GetCarts();
+        Cart GetCartById(string id, string isbn);
+        void EditCart(Cart cart);
+        void DeleteCart(Cart cart);
+        void Save();
+    }
+}
