5a4becd5c6e511ed9012658d157adcd2a1dc6342
Arsene <72069029+YoakeArsene@users.noreply.github.com>
2022-06-17 14:11:37 +0700

    Create CRUD functions for Store entity (#4)
    Co-authored-by: Anh Le Ngoc Chau <anh.le@orientsoftware.com>

---
+++ Repositories/IStoreRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Models;
+
+namespace FPTBook.Repositories
+{
+    public interface IStoreRepository
+    {
+        IEnumerable<Store> GetStores();
+        Store GetStoreById(int id);
+        void CreateStore(Store store);
+        void EditStore(Store store);
+        void DeleteStore(Store store);
+        void Save();
+    }
+}
